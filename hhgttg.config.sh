549e7508575ed78803fda365cf005bfe2982b5bc
Pavel Dolinin <pavel.dolinin@gm.com>
2025-11-26 10:15:11 +0200

    new feature of prompt configuration

--- hhgttg.config.sh
+++ hhgttg.config.sh
@@ -5,6 +5,10 @@ HHGTTG_TIMERS_SET=true
 HHGTTG_SPINNER_SET=moon
 HHGTTG_SPINNER_SPEED=0.1 # recommended speed is
 
+# Prompt theme for HHGTTG module:
+#   dontpanic | marvin | improbability | minimal | off
+HHGTTG_PROMPT_THEME="marvin" # A prompt theme
+
 HHGTTG_EXEC_DURATION_ALERT="5.0"
 
 # Just sign that this config file was loaded
